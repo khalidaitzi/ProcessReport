c721e1333f09ced7d2eddffb8a12be564f7b44e9
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-08-08 10:10:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,14 @@
 import streamlit as st
 import requests
-from xml.etree import ElementTree
 
 username = 'ait-zi.khalid@ensam-casa.ma'
 password = '$2"5EVTtb,D3xUN'
 
 def get_data():
     url = 'https://api.boomi.com/api/rest/v1/trainingkhalidaitzi-K6LQT4/ExecutionRecord/query'
-    response = requests.post(url, auth=(username, password))  
+    response = requests.post(url, auth=(username, password))
     return response.text
 
-def parse_xml(xml):
-    root = ElementTree.fromstring(xml)
-    results = []
-    for result in root.findall('bns:result', root.nsmap):
-         data = []
-         for child in result:
-             data.append(child.text)
-         results.append(data)
-    return results
-
 if __name__ == '__main__':
-    data = get_data()    
-    results = parse_xml(data) 
-    st.table(results)
+    data = get_data()
+    st.write(data)
