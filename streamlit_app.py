d9ef41deb3be31b529216c0b3d5254289198e1fe
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-08-08 10:58:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import requests
+import requests 
 import pandas as pd
 import xml.etree.ElementTree as ET
 
@@ -9,37 +9,37 @@ password = '$2"5EVTtb,D3xUN'
 st.title(":clipboard: Process reporting")
 
 def parse_xml_response(xml_response):
-    root = ET.fromstring(xml_response)
-    records = []
-    for result in root.findall('.//{http://api.platform.boomi.com/}result'):
-        record = {}
-        for child in result:
-            tag = child.tag.replace('{http://api.platform.boomi.com/}', '')
-            record[tag] = child.text
-        records.append(record)
-    return records
+  root = ET.fromstring(xml_response)
+  records = []
+  for result in root.findall('.//{http://api.platform.boomi.com/}result'):
+    record = {}
+    for child in result:
+      tag = child.tag.replace('{http://api.platform.boomi.com/}', '')  
+      record[tag] = child.text
+    records.append(record)
+  return records
 
 def get_data():
-    url = 'https://api.boomi.com/api/rest/v1/trainingkhalidaitzi-K6LQT4/ExecutionRecord/query'
-    response = requests.post(url, auth=(username, password))
-    return response.text
+  url = 'https://api.boomi.com/api/rest/v1/trainingkhalidaitzi-K6LQT4/ExecutionRecord/query'
+  response = requests.post(url, auth=(username, password))
+  return response.text
 
 if __name__ == '__main__':
-    data = get_data()
-    records = parse_xml_response(data)
-    df = pd.DataFrame(records)
+  data = get_data()
+  records = parse_xml_response(data)
+  df = pd.DataFrame(records)
+  
+  # Remove table centering
+  st.markdown('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
-    # Apply CSS styling to the table
-    st.markdown(
-        """
-        <style>
-        .full-width-table {
-            width: 100%;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+  # Add some padding 
+  st.markdown("""
+    <style>
+      .st-bo {
+        padding: 20px;  
+      }
+    </style>    
+    """, unsafe_allow_html=True)
 
-    # Render the table with the CSS class
-    st.table(df.style.set_table_attributes("class='full-width-table'"))
+  # Render as full width table
+  st.markdown(f'<div class="st-bo"><table class="full-width-table">{df.to_html(index=False)}</table></div>', unsafe_allow_html=True)
