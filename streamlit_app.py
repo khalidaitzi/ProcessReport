79ecb2e8b25287c96dbec291c0f488f31f379da0
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-08-08 10:12:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import requests
+import pandas as pd
+import xml.etree.ElementTree as ET
 
 username = 'ait-zi.khalid@ensam-casa.ma'
 password = '$2"5EVTtb,D3xUN'
+
+def parse_xml_response(xml_response):
+    root = ET.fromstring(xml_response)
+    records = []
+    for result in root.findall('.//{http://api.platform.boomi.com/}result'):
+        record = {}
+        for child in result:
+            tag = child.tag.replace('{http://api.platform.boomi.com/}', '')
+            record[tag] = child.text
+        records.append(record)
+    return records
 
 def get_data():
     url = 'https://api.boomi.com/api/rest/v1/trainingkhalidaitzi-K6LQT4/ExecutionRecord/query'
@@ -11,4 +24,6 @@ def get_data():
 
 if __name__ == '__main__':
     data = get_data()
-    st.write(data)
+    records = parse_xml_response(data)
+    df = pd.DataFrame(records)
+    st.table(df)
