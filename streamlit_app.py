8f9dd1d2bde79dfcbaa4ed2490f9761a74d98e33
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-08-08 10:01:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import requests
 
+username = 'ait-zi.khalid@ensam-casa.ma'
+password = '$2"5EVTtb,D3xUN'
+
+def get_data():
+    url = 'https://api.boomi.com/api/rest/v1/trainingkhalidaitzi-K6LQT4/ExecutionRecord/query'
+    response = requests.get(url, auth=(username, password))
+    return response.text
+
+if __name__ == '__main__':
+    data = get_data()
+    st.write(data)
