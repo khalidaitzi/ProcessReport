8ab2fffa79e8ccc2d0376b0b89435324ca77e43a
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-08-23 15:03:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,58 +1,11 @@
 import streamlit as st
-import requests 
+import requests
 import pandas as pd
 import xml.etree.ElementTree as ET
 
 username = 'ait-zi.khalid@ensam-casa.ma'
 password = '$2"5EVTtb,D3xUN'
 
-# Set Streamlit theme
-st.set_page_config(
-    page_title="Process reporting",
-    page_icon=":clipboard:",
-    layout="wide"
-)
-
-# Define custom CSS styles
-custom_styles = """
-<style>
-    /* Remove table centering */
-    div.row-widget.stRadio > div {
-        flex-direction: row;
-    }
-
-    /* Add padding and margin to table */
-    .dataframe {
-        padding: 20px;
-        margin-left: -400px;
-    }
-
-    /* Style table header */
-    .dataframe th {
-        background-color: #8684f0;
-        border: 2px solid #ddd;
-        padding: 10px;
-        text-align: left;
-    }
-
-    /* Style table cells */
-    .dataframe td {
-        border: 1px solid #ddd;
-        padding: 8px;
-    }
-
-    /* Alternate row colors */
-    .dataframe tr:nth-child(even) {
-        background-color: #8684f0;
-    }
-
-    /* Hover effect on rows */
-    .dataframe tr:hover {
-        background-color: #8684f0;
-    }
-</style>
-"""
-st.title(":clipboard: Process reporting")
 def parse_xml_response(xml_response):
     root = ET.fromstring(xml_response)
     records = []
@@ -69,13 +22,60 @@ def get_data():
     response = requests.post(url, auth=(username, password))
     return response.text
 
+# Set Streamlit theme
+st.set_page_config(
+    page_title="Process reporting",
+    page_icon=":clipboard:",
+    layout="wide"
+)
+
 if __name__ == '__main__':
     data = get_data()
     records = parse_xml_response(data)
     df = pd.DataFrame(records)
 
-    # Apply custom styles
-    st.markdown(custom_styles, unsafe_allow_html=True)
+    # Apply custom CSS styles using HTML and st.markdown
+    st.markdown(
+        """
+        <style>
+            /* Remove table centering */
+            div.row-widget.stRadio > div {
+                flex-direction: row;
+            }
+
+            /* Add padding and margin to table */
+            .dataframe {
+                padding: 20px;
+                margin-left: -400px;
+            }
+
+            /* Style table header */
+            .dataframe th {
+                background-color: #f5f5f5;
+                border: 1px solid #ddd;
+                padding: 8px;
+                text-align: left;
+            }
+
+            /* Style table cells */
+            .dataframe td {
+                border: 1px solid #ddd;
+                padding: 8px;
+            }
+
+            /* Alternate row colors */
+            .dataframe tr:nth-child(even) {
+                background-color: #f2f2f2;
+            }
+
+            /* Hover effect on rows */
+            .dataframe tr:hover {
+                background-color: #e6f7ff;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
     # Render table
     st.dataframe(df)
