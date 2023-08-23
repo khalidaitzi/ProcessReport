4a8c729cd8e285f97dbdfed807bca2022fc3a990
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-08-23 13:48:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import requests
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-username = '******'
-password = '******'
+username = 'ait-zi.khalid@ensam-casa.ma'
+password = '$2"5EVTtb,D3xUN'
 
 st.title(":clipboard: Process reporting")
 
@@ -20,7 +20,7 @@ def parse_xml_response(xml_response):
   return records
 
 def get_data():
-  url = 'https://api.boomi.com/api/rest/v1/******************/ExecutionRecord/query'
+  url = 'https://api.boomi.com/api/rest/v1/trainingkhalidaitzi-K6LQT4/ExecutionRecord/query'
   response = requests.post(url, auth=(username, password))
   return response.text
 
